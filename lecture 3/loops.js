9ca0699e927f226ca05ba34456b1286c93ad881a
Manish Baghel <manishbaghel6105@gmail.com>
2026-08-21 17:01:42 +0530

    Java Scripts code

---
+++ lecture 3/loops.js
@@ -0,0 +1,68 @@
+
+// // for loop syntax
+// // for(initizalition ,condition,updetion)
+// for(let i=1; i<=3;i++){
+//     console.log("hello world");
+// }
+// for(let i=1;i<=5;i++)
+// {
+//     console.log(i);
+// }
+// for(let i=1;i<=5;i++)
+// {
+//     console.log(i,"hello skills");
+// }
+
+// n=5
+// for(let i=1;i<=n;i++)
+// {
+//     console.log(i,"Hello world")
+// }
+
+// // while loop 
+// let i=1;
+// while(i<=5){
+//     console.log(i,"Hii");
+//     i++;
+// }
+
+// // do_while
+
+// do{
+//     console.log(1);
+//     i++;
+// }while (i<=5)
+
+// for(let i=1 ; i<=10 ; i++){
+//     if(i%2==0)
+//         console.log(i);
+// }
+
+// break and continue
+// break
+for(let i=1 ; i<=10 ; i++){
+    if(i%2==0)
+        console.log(i);
+     break;
+}
+
+// continue
+for(let i=1 ; i<=10 ; i++){
+    if(i===3 || i===7||i==9){
+     continue;
+    }
+     console.log(i);
+     break;
+}
+
+for(let i=1 ; i<=10 ; i++){
+    if(i%2 !==0){
+     continue;
+    }
+     console.log(i);
+}
+
+// write a 5 table
+for(i=0;i<=10;i++){
+    console.log(5*i);
+}
