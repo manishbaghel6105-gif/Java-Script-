67bced106058046c757698fd34dafa89e01e55be
Manish Baghel <manishbaghel6105@gmail.com>
2026-08-31 15:57:47 +0530

    JavaScript

---
+++ lecture 9/closures.js
@@ -0,0 +1,43 @@
+
+
+function outter(){
+  const a = 5;
+       function inner(){
+                    console. log(a);
+         }
+     return inner;
+}
+     const response = outter();
+          console. log(outter);
+     console. log(response());
+
+
+     // function outter(){
+//     let a = 5;
+//     function inner(){
+//         console.log(a);
+//     }
+//     a = 10;
+//     return inner;
+// }
+// const response = outter();
+// response();
+
+
+
+function outter() {
+    let count = 0;
+    function counter() {
+        count = count + 1;
+        console.log(count);
+    }
+
+    return counter
+}
+
+const counter1 = outter();
+const counter2 = outter();
+counter1();
+counter2();
+counter2();
+counter2();
